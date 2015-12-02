15efa5791eb8426a6f97b42a0134ee52b3ea2b5d
Andrew Bolster <me@andrewbolster.info>
2015-12-02 23:05:01 +0000

    glossaries and milo precommits to fix bibs

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-cd Chapters
-latexmk -interaction=nonstopmode -pdf c?-*.tex
-git add c?-*.pdf
-cd ../
+
+cp `readlink -f Thesis.lnk.bib` Thesis.bib
+git add ./Thesis.bib ./Thesis.lnk.bib
+
+if [[ "$do_chaps" -gt "0" ]]; then
+  cd Chapters
+  latexmk -interaction=nonstopmode -pdf c?-*.tex
+  git add ./c?-*.pdf
+  git add ./c?-*.dep
+  latexmk -c
+  cd ../
+fi
 
 if [[ -f "Thesis.pdf" ]]; then
   mv Thesis.pdf Thesis-Last.pdf
@@ -17,3 +25,5 @@ if [[ "$tex_exit" -gt "0" ]]; then
   exit $tex_exit 
 fi
 git add ./Thesis.pdf ./Thesis-Last.pdf
+
+git commit --amend -a --no-verify
